8785ec01399205cb2d800997ccafb6058eab0b61
Burçin Güngör <burcin.gngr.gs@gmail.com>
2025-09-05 22:55:17 +0300

    ikon güncelleme-boşluk hatası

--- components/ScreenWrapper.tsx
+++ components/ScreenWrapper.tsx
@@ -1,26 +1,40 @@
-import { Dimensions, Platform, StatusBar, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { ScreenWrapperProps } from '@/types'
-import { colors } from '@/constants/theme';
+import {
+  Dimensions,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+import { ScreenWrapperProps } from "@/types";
+import { colors } from "@/constants/theme";
 
-const {height}=Dimensions.get('window');
+const { height } = Dimensions.get("window");
 
-const ScreenWrapper = ({style,children}:ScreenWrapperProps) => {
-    let paddingTop=Platform.OS=='ios'? height * 0.06:50;
+const ScreenWrapper = ({ style, children }: ScreenWrapperProps) => {
+  let paddingTop = Platform.OS == "ios" ? height * 0.06 : 50;
   return (
-    <View style={[{
-        paddingTop,
-        flex:1,
-        backgroundColor:colors.neutral900
-
-    },
-    style]}>
-<StatusBar barStyle="light-content"/>
-  {children}
+    <View
+      style={[
+        {
+          paddingTop,
+          flex: 1,
+          backgroundColor: colors.neutral900,
+        },
+        style,
+      ]}
+    >
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      {children}
     </View>
-  )
-}
+  );
+};
 
-export default ScreenWrapper
+export default ScreenWrapper;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
